d95d10b14e1cfdce58c486fa55836cca3c11b535
unknown <anjalika.jke@gmail.com>
2025-07-26 10:42:29 +0530

    Force added full backend folder with Spring Boot project files

---
+++ backend/src/main/java/com/Timelyne/Timelyne/TimelyneApplication.java
@@ -0,0 +1,13 @@
+package com.Timelyne.Timelyne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelyneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelyneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Timelyne/Timelyne/TimelyneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Timelyne.Timelyne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelyneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
